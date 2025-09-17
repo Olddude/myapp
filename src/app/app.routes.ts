af613a8a3e916b98dac4b7dcdce4a811b370425d
Anonymous Universus <anonymous.universus@outlook.com>
2025-09-18 01:09:53 +0200

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,15 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
     {
         path: '',
-        redirectTo: 'user',
+        redirectTo: 'home',
         pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./home/home.page').then(m => m.HomePage)
     },
     {
         path: 'user',
         loadComponent: () => import('./user/user.page').then(m => m.UserPage)
-    },
-    {
-        path: 'settings',
-        loadComponent: () => import('./settings/settings.page').then(m => m.SettingsPage)
     }
 ];
