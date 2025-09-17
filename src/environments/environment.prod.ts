5bd46b7dc186df39742af550499f31860fa7d404
Anonymous Universus <anonymous.universus@outlook.com>
2025-09-17 23:57:34 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true,
+  remotes: {
+    myapp2: 'http://localhost:4201/remoteEntry.json'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: false,
+  remotes: {
+    myapp2: 'http://localhost:4201/remoteEntry.json'
+  }
 };
