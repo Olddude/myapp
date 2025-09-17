0633e528a7a8e99951bdda2826f02581b208e151
Anonymous Universus <anonymous.universus@outlook.com>
2025-09-18 00:48:31 +0200

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,16 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
     {
+        path: '',
+        redirectTo: 'user',
+        pathMatch: 'full'
+    },
+    {
         path: 'user',
         loadComponent: () => import('./user/user.page').then(m => m.UserPage)
     },
     {
         path: 'settings',
         loadComponent: () => import('./settings/settings.page').then(m => m.SettingsPage)
-    },
-    {
-        path: 'myapp2',
-        loadComponent: () => loadRemoteModule({
-            remoteEntry: 'http://localhost:4201/remoteEntry.json',
-            exposedModule: './AppComponent',
-        }).then(m => m.AppComponent)
     }
 ];
