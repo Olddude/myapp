cbe041e4cbdd4ee35eb0dbfb05027267c1593759
Konstantin Novikov <konstantin_novikov2@epam.com>
2025-09-18 17:48:53 +0200

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  imports: [
-    BrowserModule,
-    RouterModule,
-  ],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
