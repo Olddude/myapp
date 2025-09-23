d21c49b95b0d970df514269ff97b1ff0a8329184
Anonymous Universus <anonymous.universus@outlook.com>
2025-09-23 07:50:20 +0200

    asd

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   imports: [RouterModule],
-  selector: 'app-root',
+  selector: 'app-myapp-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
